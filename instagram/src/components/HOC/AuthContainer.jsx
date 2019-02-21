43973c2ca145a79cb791ab4f874261337720459f
Pav0l <pavol.karas@gmail.com>
2019-02-21 13:05:52 +0100

    Fix bug with localStorage storing userName

--- instagram/src/components/HOC/AuthContainer.jsx
+++ instagram/src/components/HOC/AuthContainer.jsx
@@ -7,11 +7,12 @@ export default function authCheck(Posts, Login) {
 
       this.state = {
         isAuth: JSON.parse(localStorage.getItem('isAuthed')) || false,
-        userName: '',
+        userName: localStorage.getItem('userName') || '',
       };
     }
 
     userNameUpdater = (event) => {
+      localStorage.setItem('userName', event.target.value);
       this.setState(({
         userName: event.target.value
       }));
@@ -19,15 +20,14 @@ export default function authCheck(Posts, Login) {
     
     buttonHandler = (event) => {
       event.preventDefault();
-      this.setState({isAuth: true,})
-      localStorage.setItem('userName', this.state.userName);
+      this.setState({isAuth: true,})      
       localStorage.setItem('isAuthed', true);
     }
 
     logOut = () => {
       localStorage.removeItem('isAuthed');
       localStorage.removeItem('userName');
-      this.setState({ isAuth: false });
+      this.setState({ isAuth: false, userName: '' });
       alert(`${this.state.userName} was logged out!`);
     }
 
